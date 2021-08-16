b0577769495339f7f55ffa65cd68fa7291711195
Konstantin Dunaev <shop.overled@gmail.com>
2021-08-16 21:57:48 +0300

    Fix bugs, add second test

--- src/test/java/ru/netology/order/FormSendTest.java
+++ src/test/java/ru/netology/order/FormSendTest.java
@@ -16,10 +16,24 @@ public class FormSendTest {
         form.$("[type=text]").setValue("John James O'Grady");
         form.$("[type=tel]").setValue("+79999999999");
         form.$("[data-test-id] [class=checkbox__box]").click();
-        form.$("[role=button]");
+        form.$("[role=button]").click();
         form.$("span[data-test-id=name] [class=input__sub]")
                 .shouldHave(exactText("Имя и Фамилия указаные неверно." +
                         " Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldSendRightData(){
+        open("http://localhost:9999");
+        SelenideElement form =$("div[class=App_appContainer__3jRx1]");
+        form.$("[type=text]").setValue("Наина Иосифовна Ельцина");
+        form.$("[type=tel]").setValue("+79999999999");
+        form.$("[data-test-id] [class=checkbox__box]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена! " +
+                        "Наш менеджер свяжется с вами в ближайшее время."));
 
     }
 
